4badaee16d8a76740da9f97d40492eddd9747e71
Sergio <sergio@Sergios-iMac.local>
2023-02-15 11:38:38 -0600

    react-3d-boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import styled from 'styled-components'
+
+function Box() {
+  return (
+    <mesh>
+      <planeGeometry args={[2, 2, 10, 10]} />
+      <meshBasicMaterial color='darkblue' wireframe />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <CanvasContainerWrapper>
+      <Canvas>
+        <ambientLight intensity={0.25} />
+        <Box />
+      </Canvas>
+    </CanvasContainerWrapper>
+  )
 }
+const CanvasContainerWrapper = styled.section`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+`
 
-export default App;
+export default App
